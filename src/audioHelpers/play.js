b2b1d141816acdeb30f309c105051d737761213d
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-04-29 17:43:31 -0700

    temp latency fix using setTimeout

--- src/audioHelpers/play.js
+++ src/audioHelpers/play.js
@@ -1,14 +1,24 @@
 import {Howl} from 'howler';
 
 const play = urls => {
-  urls.forEach(track => {
+  // urls.forEach(track => {
     var recording = new Howl({
-      src: [track],
+      src: [urls[0]],
       format: 'mp3',
       html5: true
     });
-    recording.play();
-  });
+
+    var recording2 = new Howl({
+      src: [urls[1]],
+      format: 'mp3',
+      html5: true
+    });
+
+    setTimeout(() => {
+      recording.play();
+    }, 450);
+    recording2.play();
+  // });
 }
 
 export default play;
